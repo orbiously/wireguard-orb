af44427b8e8cd14791d70d4e90bc263e254e9933
Yann <yann.domingo@gmail.com>
2022-05-01 21:45:28 +0100

    Refactoring

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-install() {
+identify() {
   case $1 in
     [Ll]inux*)
       if [ -f /.dockerenv ]; then
@@ -8,28 +8,38 @@ install() {
       else
         EXECUTOR=linux
       fi
-      printf "Installing WireGuard for Linux\n\n"
-      sudo apt-get update
-      sudo apt-get install -y wireguard-tools resolvconf
       PLATFORM=Linux
       ;;
     [Dd]arwin*)
-      printf "Installing WireGuard for macOS\n\n"
-      HOMEBREW_NO_AUTO_UPDATE=1 brew install wireguard-tools
-      sudo sed -i '' 's/\/usr\/bin\/env[[:space:]]bash/\/usr\/local\/bin\/bash/' /usr/local/Cellar/wireguard-tools/1.0.20210914/bin/wg-quick
       PLATFORM=macOS
       EXECUTOR=macos
       ;;
     msys*|MSYS*|nt|win*)
-      printf "Installing WireGuard for Windows\n\n"
-      choco install wireguard
       PLATFORM=Windows
       EXECUTOR=windows
       ;;
   esac
 }
 
-install "$(uname)"
+install-Linux() {
+  printf "Installing WireGuard for Linux\n\n"
+  sudo apt-get update
+  sudo apt-get install -y wireguard-tools resolvconf
+}
+
+install-macOS() {
+  printf "Installing WireGuard for macOS\n\n"
+  HOMEBREW_NO_AUTO_UPDATE=1 brew install wireguard-tools
+  sudo sed -i '' 's/\/usr\/bin\/env[[:space:]]bash/\/usr\/local\/bin\/bash/' /usr/local/Cellar/wireguard-tools/1.0.20210914/bin/wg-quick
+}
+
+install-Windows() {
+  printf "Installing WireGuard for Windows\n\n"
+  choco install wireguard
+}
+
+identify
+install-$PLATFORM
 
 printf "\nWireGuard for %s installed\n\n" "$PLATFORM"
 printf "\nPublic IP before VPN connection is %s\n" "$(curl http://checkip.amazonaws.com)"
