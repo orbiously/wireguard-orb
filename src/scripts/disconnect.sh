dfcacb3fd7f4096e283c3c5f29021aff3056979c
Yann <yann.domingo@gmail.com>
2022-05-11 00:44:54 +0100

    Cleaning up after disconnection

--- src/scripts/disconnect.sh
+++ src/scripts/disconnect.sh
@@ -2,11 +2,14 @@
 case "$EXECUTOR" in
   linux)
     sudo wg-quick down wg0
+    sudo rm -f /etc/wireguard/wg0.conf
     ;;
   macos)
     sudo wg-quick down /tmp/wg0.conf
+    rm -f /tmp/wg0.conf
     ;;
   windows)
     /c/progra~1/wireguard/wireguard.exe //uninstalltunnelservice wg0
+    rm -f "C:\tmp\wg0.conf"
     ;;
 esac
