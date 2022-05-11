b7b65dbd8f2341559d581e2da861ab032d2d3b2c
Yann <yann.domingo@gmail.com>
2022-05-11 01:29:47 +0100

    Ensuring success of disconnection & cleanup

--- src/scripts/disconnect.sh
+++ src/scripts/disconnect.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 case "$EXECUTOR" in
   linux)
-    sudo wg-quick down wg0
-    sudo rm -f /etc/wireguard/wg0.conf
+    disconnect_command=(sudo wg-quick down wg0)
+    cleanup_command=(sudo rm -f /etc/wireguard/wg0.conf)
     ;;
   macos)
-    sudo wg-quick down /tmp/wg0.conf
-    rm -f /tmp/wg0.conf
+    disconnect_command=(sudo wg-quick down /tmp/wg0.conf)
+    cleanup_command=(rm -f /tmp/wg0.conf)
     ;;
   windows)
-    /c/progra~1/wireguard/wireguard.exe //uninstalltunnelservice wg0
-    rm -f "C:\tmp\wg0.conf"
+    disconnect_command=(/c/progra~1/wireguard/wireguard.exe //uninstalltunnelservice wg0)
+    cleanup_command=(rm -f "C:\tmp\wg0.conf")
     ;;
 esac
+
+if "${disconnect_command[@]}"; then
+  printf "Disconnected from WireGuard server\n"
+fi
+
+if "${cleanup_command[@]}"; then
+  printf "Configuration deleted\n"
+fi
